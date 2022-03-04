ba40f5e30ec92cc7779a2e4f10eec3b3a2b8a845
pauliescanlon <paul.scanlon@gatsbyjs.com>
2022-03-04 11:43:41 +0000

    feat: test urls

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,12 +1,20 @@
 const path = require('path');
 
-exports.onPreBuild = async ({ actions }) => {
+exports.onPreBuild = async ({ actions: { createRedirect } }) => {
   const { copyLibFiles } = await import('@builder.io/partytown/utils');
   copyLibFiles(path.join(__dirname, 'static', '~partytown'));
 
-  actions.createRedirect({
-    fromPath: `/_partytown?url=:url`,
+  createRedirect({
+    fromPath: `/google-analytics?url=:url`,
     toPath: `:url`,
+    statusCode: 200
+  });
+};
+
+exports.createPages = async ({ graphql, actions: { createRedirect } }) => {
+  createRedirect({
+    fromPath: `/foo/test`,
+    toPath: `/bar/test`,
     statusCode: 200
   });
 };
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -6,33 +6,31 @@ export const onRenderBody = ({ setHeadComponents }) => {
     <Partytown
       key="partytown"
       debug={true}
-      resolveUrl={(url, location, method) => {
-        if (method !== 'GET') {
-          return url;
-        }
+      resolveUrl={(url, location) => {
+        console.log('url.href: ', url.href);
 
         if (url.hostname === 'www.google-analytics.com') {
-          var proxyUrl = new URL(location.origin);
-          proxyUrl.pathname = `/_partytown`;
-          proxyUrl.search = url.search;
-          proxyUrl.append('url', url.href);
+          var proxyUrl = new URL(`${location.origin}/google-analtyics`);
+          proxyUrl.searchParams.append('url', url.href);
+          console.log('proxyUrl: ', proxyUrl);
           return proxyUrl;
         }
-
         return url;
       }}
     />,
+    // https://developers.google.com/analytics/devguides/collection/analyticsjs
+    // https://gatsbythirdpartyscripts-featpartytown.gtsb.io/
     <script
       key="google-analytics"
       type="text/partytown"
       dangerouslySetInnerHTML={{
         __html: `(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-            
-            ga('create', 'UA-76055934-4', 'auto');
-            ga('send', 'pageview');`
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-76055934-15', 'auto');
+        ga('send', 'pageview');`
       }}
     />
   ]);
