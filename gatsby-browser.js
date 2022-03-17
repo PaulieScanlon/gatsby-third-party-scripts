99a66c0b1db7f7a42265fd673c795929ab91b78c
pauliescanlon <paul.scanlon@gatsbyjs.com>
2022-03-17 13:51:38 +0000

    feat: add onRouteUpdate

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,7 @@
 export const onRouteUpdate = ({ location }) => {
-  // console.log('process.env.GATSBY_GA_MEASUREMENT_ID: ', process.env.GATSBY_GA_MEASUREMENT_ID);
-  // console.log('location: ', location);
-  // window.ga('create', `'${process.env.GATSBY_GA_MEASUREMENT_ID}'`, 'auto');
-  // window.gtag(`event`, `page_view`, { page_path: location });
+  console.log('process.env.GATSBY_GA_MEASUREMENT_ID: ', process.env.GATSBY_GA_MEASUREMENT_ID);
+  console.log('location: ', location);
+
+  window.ga('create', `'${process.env.GATSBY_GA_MEASUREMENT_ID}'`, 'auto');
+  window.gtag(`event`, `page_view`, { page_path: location });
 };
