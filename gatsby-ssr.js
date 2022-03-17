2bbbe7cc2dfa385ef05f070ba1f54ecc1ee98d3d
pauliescanlon <paul.scanlon@gatsbyjs.com>
2022-03-17 11:31:23 +0000

    feat: add GA4 tracking code

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -17,15 +17,7 @@ export const onRenderBody = ({ setHeadComponents }) => {
     <script
       key="google-analytics"
       type="text/partytown"
-      dangerouslySetInnerHTML={{
-        __html: `(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-        ga('create', 'UA-76055934-15', 'auto');
-        ga('send', 'pageview');`
-      }}
+      src="https://www.googletagmanager.com/gtag/js?id=G-94MF2D2CTG"
     />
   ]);
 };
