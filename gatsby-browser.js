7ee70dfa39edd031c771bafee30981332f6b57bb
pauliescanlon <paul.scanlon@gatsbyjs.com>
2022-03-17 13:59:49 +0000

    feat: add setTimeout

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,6 +2,8 @@ export const onRouteUpdate = ({ location }) => {
   console.log('process.env.GATSBY_GA_MEASUREMENT_ID: ', process.env.GATSBY_GA_MEASUREMENT_ID);
   console.log('location: ', location);
 
-  window.ga('create', `'${process.env.GATSBY_GA_MEASUREMENT_ID}'`, 'auto');
-  window.gtag(`event`, `page_view`, { page_path: location });
+  setTimeout(() => {
+    window.ga('create', `'${process.env.GATSBY_GA_MEASUREMENT_ID}'`, 'auto');
+    window.gtag(`event`, `page_view`, { page_path: location });
+  }, 2000);
 };
