5d94faea36aa935e87dc2738f407f3a16bc0ee71
pauliescanlon <paul.scanlon@gatsbyjs.com>
2022-03-17 14:03:12 +0000

    feat: remove create event

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,7 +3,7 @@ export const onRouteUpdate = ({ location }) => {
   console.log('location: ', location);
 
   setTimeout(() => {
-    window.ga('create', `'${process.env.GATSBY_GA_MEASUREMENT_ID}'`, 'auto');
+    // window.gtag('create', `'${process.env.GATSBY_GA_MEASUREMENT_ID}'`, 'auto');
     window.gtag(`event`, `page_view`, { page_path: location });
   }, 2000);
 };
