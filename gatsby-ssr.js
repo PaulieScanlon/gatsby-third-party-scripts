0456ad38c7e807aa1ad7fd2a040acb69a5cf9fc7
pauliescanlon <paul.scanlon@gatsbyjs.com>
2022-03-17 13:33:59 +0000

    feat: add Partytown back in

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -8,30 +8,24 @@ export const onRenderBody = ({ setHeadComponents, setPostBodyComponents }) => {
 
   setHeadComponents([
     <link key="preconnect" rel="preconnect" key="preconnect-google-gtag" href={origin} />,
-    <link key="dns-prefetch" rel="dns-prefetch" key="dns-prefetch-google-gtag" href={origin} />
-    // <Partytown
-    //   key="partytown"
-    //   resolveUrl={(url, location) => {
-    //     if (url.hostname === 'www.googletagmanager.com' && url.pathname.endsWith('.js')) {
-    //       var proxyUrl = new URL(`${location.origin}/google-analytics`);
-    //       proxyUrl.searchParams.append('url', url.href);
-    //       return proxyUrl;
-    //     }
-    //     return url;
-    //   }}
-    // />,
-    // <script
-    //   key="google-analytics"
-    //   // type="text/partytown"
-    //   src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GATSBY_GA_MEASUREMENT_ID}`}
-    // />
+    <link key="dns-prefetch" rel="dns-prefetch" key="dns-prefetch-google-gtag" href={origin} />,
+    <Partytown
+      key="partytown"
+      // resolveUrl={(url, location) => {
+      //   if (url.hostname === 'www.googletagmanager.com' && url.pathname.endsWith('.js')) {
+      //     var proxyUrl = new URL(`${location.origin}/google-analytics`);
+      //     proxyUrl.searchParams.append('url', url.href);
+      //     return proxyUrl;
+      //   }
+      //   return url;
+      // }}
+    />
   ]);
 
   setPostBodyComponents([
     <script
       key="google-analytics"
-      async
-      // type="text/partytown"
+      type="text/partytown"
       src={`${origin}/gtag/js?id=${process.env.GATSBY_GA_MEASUREMENT_ID}`}
     />,
     <script
