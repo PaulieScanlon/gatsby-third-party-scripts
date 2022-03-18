e689957ca3b8d45bf1fb6d1398d80e87836df940
pauliescanlon <paul.scanlon@gatsbyjs.com>
2022-03-18 12:37:09 +0000

    feat: remove createRedirect

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,10 +3,4 @@ const { copyLibFiles } = require('@builder.io/partytown/utils');
 
 exports.onPreBuild = async ({ actions: { createRedirect } }) => {
   copyLibFiles(path.join(__dirname, 'static', '~partytown'));
-
-  createRedirect({
-    fromPath: `/google-analytics?url=:url`,
-    toPath: `:url`,
-    statusCode: 200
-  });
 };
