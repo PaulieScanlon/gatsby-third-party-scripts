d52c73be676d8d51522834a414d4995c373579b6
pauliescanlon <paul.scanlon@gatsbyjs.com>
2022-03-18 09:32:48 +0000

    chore: remove onRouteUpdate

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,9 +1,9 @@
-export const onRouteUpdate = ({ location }) => {
-  console.log('process.env.GATSBY_GA_MEASUREMENT_ID: ', process.env.GATSBY_GA_MEASUREMENT_ID);
-  console.log('location: ', location);
+// export const onRouteUpdate = ({ location }) => {
+//   console.log('process.env.GATSBY_GA_MEASUREMENT_ID: ', process.env.GATSBY_GA_MEASUREMENT_ID);
+//   console.log('location: ', location);
 
-  setTimeout(() => {
-    // window.gtag('create', `'${process.env.GATSBY_GA_MEASUREMENT_ID}'`, 'auto');
-    window.gtag(`event`, `page_view`, { page_path: location });
-  }, 2000);
-};
+//   setTimeout(() => {
+//     window.gtag('create', `'${process.env.GATSBY_GA_MEASUREMENT_ID}'`, 'auto');
+//     window.gtag(`event`, `page_view`, { page_path: location });
+//   }, 2000);
+// };
