5d97e5a40ed26a993ae1018a3459da8baa306bcb
pauliescanlon <paul.scanlon@gatsbyjs.com>
2022-03-18 10:36:53 +0000

    test: add DSG page

---
+++ src/pages/test.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Page = () => {
+  return (
+    <main>
+      <h1>
+        Performance Optimization for third-party scripts{' '}
+        <span role="img" aria-label="Party Popper">
+          🎉
+        </span>
+      </h1>
+      <p>
+        third-party scripts:{' '}
+        <a target="_blank" rel="noreferrer" href="https://partytown.builder.io/">
+          Partytown
+        </a>
+      </p>
+    </main>
+  );
+};
+
+export const config = () => {
+  return () => ({
+    defer: true
+  });
+};
+
+export default Page;
