548e45cf3455191258b692b707558c71a7601bf3
pauliescanlon <paul.scanlon@gatsbyjs.com>
2022-03-18 09:56:32 +0000

    chore: cleanup

--- gatsby-browser.js
+++
@@ -1,9 +0,0 @@
-// export const onRouteUpdate = ({ location }) => {
-//   console.log('process.env.GATSBY_GA_MEASUREMENT_ID: ', process.env.GATSBY_GA_MEASUREMENT_ID);
-//   console.log('location: ', location);
-
-//   setTimeout(() => {
-//     window.gtag('create', `'${process.env.GATSBY_GA_MEASUREMENT_ID}'`, 'auto');
-//     window.gtag(`event`, `page_view`, { page_path: location });
-//   }, 2000);
-// };
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,30 +1,15 @@
 import React from 'react';
 import { Partytown } from '@builder.io/partytown/react';
 
-export const onRenderBody = ({ setHeadComponents, setPostBodyComponents }) => {
-  console.log('process.env.GATSBY_GA_MEASUREMENT_ID: ', process.env.GATSBY_GA_MEASUREMENT_ID);
+const ORIGIN = 'https://www.googletagmanager.com';
+const GATSBY_GA_MEASUREMENT_ID = 'G-94MF2D2CTG';
 
-  const origin = 'https://www.googletagmanager.com';
-
+export const onRenderBody = ({ setHeadComponents }) => {
   setHeadComponents([
-    <link key="preconnect" rel="preconnect" key="preconnect-google-gtag" href={origin} />,
-    <link key="dns-prefetch" rel="dns-prefetch" key="dns-prefetch-google-gtag" href={origin} />,
-    <Partytown
-      key="partytown"
-      // resolveUrl={(url, location) => {
-      //   if (url.hostname === 'www.googletagmanager.com' && url.pathname.endsWith('.js')) {
-      //     var proxyUrl = new URL(`${location.origin}/google-analytics`);
-      //     proxyUrl.searchParams.append('url', url.href);
-      //     return proxyUrl;
-      //   }
-      //   return url;
-      // }}
-    />,
-    <script
-      key="google-analytics"
-      type="text/partytown"
-      src={`${origin}/gtag/js?id=${process.env.GATSBY_GA_MEASUREMENT_ID}`}
-    />,
+    <link key="preconnect" rel="preconnect" key="preconnect-google-gtag" href={ORIGIN} />,
+    <link key="dns-prefetch" rel="dns-prefetch" key="dns-prefetch-google-gtag" href={ORIGIN} />,
+    <Partytown key="partytown" />,
+    <script key="google-analytics" type="text/partytown" src={`${ORIGIN}/gtag/js?id=${GATSBY_GA_MEASUREMENT_ID}`} />,
     <script
       key="google-analytics-config"
       type="text/partytown"
@@ -33,23 +18,8 @@ export const onRenderBody = ({ setHeadComponents, setPostBodyComponents }) => {
         function gtag(){dataLayer.push(arguments);}
 
         gtag('js', new Date()); 
-        gtag('config', '${process.env.GATSBY_GA_MEASUREMENT_ID}', { page_path: window.location.pathname })
-        
-        `
+        gtag('config', '${GATSBY_GA_MEASUREMENT_ID}', { page_path: window.location.pathname })`
       }}
     />
-  ]);
-
-  setPostBodyComponents([
-    // <script
-    //   key="google-analytics-config"
-    //   dangerouslySetInnerHTML={{
-    //     __html: `window.dataLayer = window.dataLayer || [];
-    //       function gtag(){dataLayer.push(arguments);}
-    //       gtag('js', new Date());
-    //       gtag('config', '${process.env.GATSBY_GA_MEASUREMENT_ID}', { page_path: window.location.pathname })
-    //       `
-    //   }}
-    // />
   ]);
 };
