ab68d4650c0d5a40b9097d8f071a268041c027e4
pauliescanlon <paul.scanlon@gatsbyjs.com>
2022-03-21 12:12:52 +0000

    chore: cleanup

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const { copyLibFiles } = require('@builder.io/partytown/utils');
 
-exports.onPreBuild = async ({ actions: { createRedirect } }) => {
-  copyLibFiles(path.join(__dirname, 'static', '~partytown'));
+exports.onPreBuild = async () => {
+  await copyLibFiles(path.join(__dirname, 'static', '~partytown'));
 };
