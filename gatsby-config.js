c5de97f3174a4f5d7fdb782e2faa669a67ac3a0d
pauliescanlon <paul.scanlon@gatsbyjs.com>
2022-03-21 13:26:47 +0000

    feat: remove gatsby-plugin-gatsby-cloud

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,5 @@
-require('dotenv').config({
-  path: `.env.${process.env.NODE_ENV}`
-});
-
 module.exports = {
   siteMetadata: {
     title: 'Gatsby thrid-party scripts'
-  },
-  plugins: ['gatsby-plugin-gatsby-cloud']
+  }
 };
